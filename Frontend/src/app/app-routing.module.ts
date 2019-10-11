351f0b051a291ff757d3def15d34aa47759b9282
piumikahansini <36619968+piumikahansini@users.noreply.github.com>
2019-10-11 12:00:47 +0530

    created new components

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {AboutUsComponent} from './about-us/about-us.component';
+import {ServicesComponent} from './services/services.component';
+import {ContactUsComponent} from './contact-us/contact-us.component';
+import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '',component: HomeComponent},
+  {path: 'aboutus',component: AboutUsComponent},
+  {path: 'services',component: ServicesComponent},
+  {path: 'contactus',component: ContactUsComponent},
+  {path: 'login',component: LoginComponent},
+  {path: 'register',component: RegisterComponent}
+  
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -4,11 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    ServicesComponent,
+    AboutUsComponent,
+    ContactUsComponent,
+    LoginComponent,
+    RegisterComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
